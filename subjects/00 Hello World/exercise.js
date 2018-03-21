c533f883afebcc3e21aec6417b5868e37283e885
Steve Skladany <steve@skladany.net>
2018-03-21 11:01:15 -0400

    Hello world lecture...

--- subjects/00 Hello World/exercise.js
+++ subjects/00 Hello World/exercise.js
@@ -7,8 +7,33 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-function App() {
-  return <div>Hello world!</div>;
+const node = document.getElementById("app")
+
+const people = [
+	{ firstName: "Michael", lastName: "Jackson"},
+	{ firstName: "Home", lastName: "Simpson"},
+];
+
+function PeopleList(props) {
+	const people = props.people;
+
+	return (
+		<ul className={props.className} >
+		{people.map((person, index) => (
+			<li>
+				{index+1} {person.firstName} {person.lastName}
+			</li>
+		))}
+		</ul>
+	);
 }
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const element = <PeopleList className="hot" people={people} />;
+
+ReactDOM.render(element,node);
+
+// function App() {
+//   return <div>Hello world!</div>;
+// }
+
+// ReactDOM.render(<App />, document.getElementById("app"));
